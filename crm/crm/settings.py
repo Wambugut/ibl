94fa3a4aceb9886ae7585b642b075aef23895b3c
wambugut <wambuguthomas01@gmail.com>
2024-05-22 13:39:04 +0300

    commit 2

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -75,12 +75,33 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'blog',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Sirtom@2002',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
 
 
 # Password validation
